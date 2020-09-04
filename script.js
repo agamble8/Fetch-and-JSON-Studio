3274129a9ec5e5febb97b5ad24e62ce7417fe3c3
Amy Gamble <amymgambl@gmail.com>
2020-09-03 20:57:04 -0500

    finished index and script and style for studio

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-// TODO: add code here
+  window.addEventListener("load", function() {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+       response.json().then(function(json) {
+        //  console.log(json);
+        let astroCount = 0;
+         const container = document.getElementById("container");
+         for (let i = 0; i<json.length; i++){
+            container.innerHTML += `
+            <div class="astronaut">
+                <div class="bio">
+                    <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                    <ul>
+                        <li>Hours in space: ${json[i].hoursInSpace}</li>
+                        <li id="active" active-status=${json[i].active}>Active: ${json[i].active}</li>
+                        <li>Skills: ${json[i].skills}</li>
+                    </ul>
+                </div>
+                <img class="avatar" src=${json[i].picture}></img>
+            </div>
+            `;
+            
+            if (json[i].active == true){
+              astroCount+=1;
+            };
+          };
+        container.innerHTML += `<h3>Active Astronauts: ${astroCount}</h3>`;
+       });
+    });
+
+ });
